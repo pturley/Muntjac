8597e4b0bb856a13256e44b6bab421a457fb608e
Derek Hammer <derek.r.hammer@gmail.com>
2011-06-05 16:08:59 -0500

    Refactoring.

--- muntjac.rb
+++ muntjac.rb
@@ -3,6 +3,7 @@
 team_file = ".git/team.txt"
 
 def display_help?
+  team_file = ".git/team.txt"
   ARGV.length > 1 or
   (ARGV.length == 0 and !File.exists?(team_file)) or
   ARGV[1] == 'help'
@@ -23,11 +24,8 @@ File.open(team_file, "wb") { |file| file << team }
 full_log = `git log`
 commits = full_log.split(/^commit/)
 
-matched_commits = []
-team.split(",").each do |team_member|
-  commits.each do |commit|
-    matched_commits << commit if Regexp.new(team_member.strip, true).match(commit)
-  end
+matched_commits = team.split(",").inject [] do |matches, team_member|
+  matches.concat commits.select { |commit| Regexp.new(team_member.strip, true).match(commit) }
 end
 
 puts matched_commits
