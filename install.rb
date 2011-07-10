f039b04ab02af154f02c0b326c0de8e69cfe3a7e
Patrick Turley [pturley] <patrick.turley@gmail.com>
2011-07-10 18:28:45 -0500

    Making the install script remove the old symlink before trying to build a new one

--- install.rb
+++ install.rb
@@ -4,4 +4,5 @@ git = `which git`.chomp.gsub(/\/git$/, "")
 muntjac_src = File.expand_path 'muntjac.rb'
 git_muntjac = git + '/git-muntjac'
 
+`rm #{git_muntjac}`
 `ln -s #{muntjac_src} #{git_muntjac}`
